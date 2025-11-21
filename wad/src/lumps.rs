0d70370558021eddea402d6164eb1b960b7fe2ef
Ryan Thomson <git@ryanthomson.net>
2025-11-20 21:29:25 -0500

    Refactor lump parsing to use LumpParser

--- wad/src/lumps.rs
+++ wad/src/lumps.rs
@@ -1,3 +1,4 @@
+use crate::lump_parser::{LumpParser, Result};
 use crate::WadString;
 
 pub struct PlaypalLump {
@@ -5,17 +6,18 @@ pub struct PlaypalLump {
 }
 
 impl PlaypalLump {
-	pub fn from_bytes(data: &[u8]) -> Result<Self, ()> {
-		let (chunks, []) = data.as_chunks::<{ playpal::Palette::SIZE_BYTES }>() else {
-			return Err(());
-		};
+	pub fn parse(data: &[u8]) -> Result<Self> {
+		let mut parser = LumpParser::new(&data);
 
-		Ok(PlaypalLump {
-			palettes: chunks
-				.iter()
-				.map(playpal::Palette::from_bytes)
-				.collect::<Vec<_>>(),
-		})
+		let mut palettes = Vec::with_capacity(data.len() / playpal::Palette::BYTES);
+		while parser.has_data_left() {
+			let bytes = parser.read_chunk::<{ playpal::Palette::BYTES }>()?;
+			palettes.push(playpal::Palette::from_bytes(&bytes));
+		}
+
+		// has_data_left() must be false, so no need to finish()
+
+		Ok(PlaypalLump { palettes })
 	}
 }
 
@@ -27,12 +29,10 @@ pub mod playpal {
 
 	impl Palette {
 		pub const NUM_COLORS: usize = 256;
-		pub const SIZE_BYTES: usize = Self::NUM_COLORS * Color::SIZE_BYTES;
+		pub const BYTES: usize = Self::NUM_COLORS * Color::BYTES;
 
-		pub fn from_bytes(data: &[u8; Self::SIZE_BYTES]) -> Self {
-			let (chunks, []) = data.as_chunks::<{ Color::SIZE_BYTES }>() else {
-				unreachable!()
-			};
+		pub fn from_bytes(bytes: &[u8; Self::BYTES]) -> Self {
+			let (chunks, _) = bytes.as_chunks::<3>();
 
 			Self {
 				colors: chunks
@@ -53,9 +53,9 @@ pub mod playpal {
 	}
 
 	impl Color {
-		pub const SIZE_BYTES: usize = 3;
+		pub const BYTES: usize = 3;
 
-		pub fn from_bytes(data: &[u8; Self::SIZE_BYTES]) -> Self {
+		pub fn from_bytes(data: &[u8; Self::BYTES]) -> Self {
 			Self {
 				r: data[0],
 				g: data[1],
@@ -80,21 +80,22 @@ impl ColormapLump {
 	pub const NUM_MAPS: usize = 34;
 	pub const INVULN_INDEX: usize = 32;
 
-	pub fn from_bytes(data: &[u8]) -> Result<Self, ()> {
-		if data.len() != playpal::Palette::NUM_COLORS * Self::NUM_MAPS {
-			return Err(());
-		}
+	pub fn parse(data: &[u8]) -> Result<Self> {
+		let mut parser = LumpParser::new(&data);
 
-		let (chunks, []) = data.as_chunks::<{ playpal::Palette::NUM_COLORS }>() else {
-			unreachable!()
-		};
+		let maps: Vec<_> = (0..Self::NUM_MAPS)
+			.map(|_| parser.read_chunk::<{ playpal::Palette::NUM_COLORS }>())
+			.collect::<Result<_>>()?;
+
+		parser.finish()?;
 
 		Ok(Self {
-			maps: chunks.try_into().unwrap(),
+			maps: maps.try_into().unwrap(),
 		})
 	}
 }
 
+/// The TEXTURE1 and TEXTURE2 lumps.
 pub struct TexturesLump {
 	pub num_textures: i32,
 	pub offsets: Vec<i32>,
@@ -102,24 +103,22 @@ pub struct TexturesLump {
 }
 
 impl TexturesLump {
-	pub fn from_bytes(data: &[u8]) -> Result<Self, ()> {
-		// TODO: verify the length of the input data is valid
-		let num_textures = i32::from_le_bytes(data[0..4].try_into().map_err(|_| ())?);
+	pub fn parse(data: &[u8]) -> Result<Self> {
+		let mut parser = LumpParser::new(&data);
 
-		let (chunks, []) = data[4..(4 + 4 * num_textures as usize)].as_chunks::<4>() else {
-			unreachable!()
-		};
+		let num_textures = parser.read_i32()?;
 
-		let offsets: Vec<i32> = chunks
-			.iter()
-			.map(|bytes| i32::from_le_bytes(*bytes))
-			.collect();
+		let offsets: Vec<i32> = (0..num_textures)
+			.map(|_| parser.read_i32())
+			.collect::<Result<_>>()?;
 
+		// TODO: Evaluate the best parsing strategy here. There are a few ways you
+		//  could go about this, depending on the assumptions you make about WADs.
+		//  Mainly, are the TexEntries always contiguous in the lump?
 		let textures: Vec<textures::TexEntry> = offsets
 			.iter()
-			.map(|offset| textures::TexEntry::from_bytes(&data[(*offset as usize)..]))
-			.collect::<Result<_, _>>()
-			.map_err(|_| ())?;
+			.map(|offset| textures::TexEntry::parse(&data[(*offset as usize)..]))
+			.collect::<Result<_>>()?;
 
 		Ok(Self {
 			num_textures,
@@ -130,7 +129,7 @@ impl TexturesLump {
 }
 
 pub mod textures {
-	use crate::wad::WadString;
+	use super::{LumpParser, Result, WadString};
 
 	pub struct TexEntry {
 		pub name: WadString,
@@ -143,22 +142,33 @@ pub mod textures {
 	}
 
 	impl TexEntry {
-		pub fn from_bytes(data: &[u8]) -> Result<Self, ()> {
-			let num_patches = i16::from_le_bytes(data[20..22].try_into().map_err(|_| ())?);
-			let patches_slice = &data[22..(22 + Patch::SIZE_BYTES * num_patches as usize)];
+		pub fn parse(data: &[u8]) -> Result<Self> {
+			let mut parser = LumpParser::new(&data);
 
-			let (chunks, []) = patches_slice.as_chunks::<{ Patch::SIZE_BYTES }>() else {
-				unreachable!()
-			};
+			let name = WadString::from_bytes(parser.read_chunk::<8>()?)?;
+			let _masked = parser.read_i32()?;
+			let tex_width = parser.read_i16()?;
+			let tex_height = parser.read_i16()?;
+			let _columndirectory = parser.read_i32()?;
+			let num_patches = parser.read_i16()?;
+
+			let patches: Vec<Patch> = (0..num_patches)
+				.map(|_| {
+					let bytes = parser.read_chunk::<10>()?;
+					Ok(Patch::from_bytes(&bytes))
+				})
+				.collect::<Result<_>>()?;
+
+			// For now we intentionally don't call finish()
 
 			Ok(Self {
-				name: WadString::from_bytes(data[0..8].try_into().map_err(|_| ())?)?,
-				_masked: i32::from_le_bytes(data[8..12].try_into().map_err(|_| ())?),
-				tex_width: i16::from_le_bytes(data[12..14].try_into().map_err(|_| ())?),
-				tex_height: i16::from_le_bytes(data[14..16].try_into().map_err(|_| ())?),
-				_columndirectory: i32::from_le_bytes(data[16..20].try_into().map_err(|_| ())?),
+				name,
+				_masked,
+				tex_width,
+				tex_height,
+				_columndirectory,
 				num_patches,
-				patches: chunks.iter().map(Patch::from_bytes).collect(),
+				patches,
 			})
 		}
 	}
@@ -172,17 +182,37 @@ pub mod textures {
 	}
 
 	impl Patch {
-		const SIZE_BYTES: usize = 10;
+		const NUM_BYTES: usize = 10;
 
-		pub fn from_bytes(data: &[u8; Self::SIZE_BYTES]) -> Self {
+		pub fn from_bytes(bytes: &[u8; Self::NUM_BYTES]) -> Self {
 			Self {
-				x_offset: i16::from_le_bytes(data[0..2].try_into().unwrap()),
-				y_offset: i16::from_le_bytes(data[2..4].try_into().unwrap()),
-				pname_index: i16::from_le_bytes(data[4..6].try_into().unwrap()),
-				_stepdir: i16::from_le_bytes(data[6..8].try_into().unwrap()),
-				_colormap: i16::from_le_bytes(data[8..10].try_into().unwrap()),
+				x_offset: i16::from_le_bytes(bytes[0..2].try_into().unwrap()),
+				y_offset: i16::from_le_bytes(bytes[2..4].try_into().unwrap()),
+				pname_index: i16::from_le_bytes(bytes[4..6].try_into().unwrap()),
+				_stepdir: i16::from_le_bytes(bytes[6..8].try_into().unwrap()),
+				_colormap: i16::from_le_bytes(bytes[8..10].try_into().unwrap()),
 			}
 		}
+	}
+}
+
+pub struct PnamesLump {
+	pub pnames: Vec<WadString>,
+}
+
+impl PnamesLump {
+	pub fn parse(data: &[u8]) -> Result<Self> {
+		let mut parser = LumpParser::new(&data);
+
+		let num_patches = parser.read_i32()?;
+
+		let pnames: Vec<WadString> = (0..num_patches)
+			.map(|_| WadString::from_bytes(parser.read_chunk::<8>()?))
+			.collect::<Result<_>>()?;
+
+		parser.finish()?;
+
+		Ok(Self { pnames })
 	}
 }
 
@@ -196,30 +226,5 @@ mod tests {
 		assert_eq!(color.r, 255);
 		assert_eq!(color.g, 159);
 		assert_eq!(color.b, 67);
-	}
-}
-
-pub struct PnamesLump {
-	pub pnames: Vec<WadString>,
-}
-
-impl PnamesLump {
-	pub fn parse(data: &[u8]) -> Result<Self, ()> {
-		let num_patches = i32::from_le_bytes(data[0..4].try_into().unwrap());
-
-		if data.len() != 4 + (num_patches as usize) * WadString::SIZE_BYTES {
-			return Err(());
-		}
-
-		let (chunks, []) = data[4..].as_chunks::<{ WadString::SIZE_BYTES }>() else {
-			unreachable!()
-		};
-
-		let pnames: Vec<WadString> = chunks
-			.iter()
-			.map(|bytes| WadString::from_bytes(*bytes).unwrap())
-			.collect();
-
-		Ok(Self { pnames })
 	}
 }
