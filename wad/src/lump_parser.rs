11acbee2d9ff76928f2bc78314c0b6cf36c7b505
Ryan Thomson <git@ryanthomson.net>
2025-12-09 21:41:32 -0500

    Add more LumpParser methods

--- wad/src/lump_parser.rs
+++ wad/src/lump_parser.rs
@@ -23,6 +23,16 @@ impl<'a> LumpParser<'a> {
 		Self { remaining: data }
 	}
 
+	/// Takes the next N bytes without returning anything.
+	pub fn read_padding(&mut self, n: usize) -> Result<()> {
+		let Some(rest) = self.remaining.get(n..) else {
+			self.remaining = &[];
+			return Err(ParseError::EndOfLump);
+		};
+		self.remaining = rest;
+		Ok(())
+	}
+
 	/// Takes the next N bytes as a non-copied slice.
 	pub fn read_slice(&mut self, n: usize) -> Result<&[u8]> {
 		let Some((chunk, rest)) = self.remaining.split_at_checked(n) else {
@@ -39,16 +49,34 @@ impl<'a> LumpParser<'a> {
 		Ok(slice.try_into().unwrap())
 	}
 
+	/// Takes the next byte as an unsigned integer.
+	pub fn read_u8(&mut self) -> Result<u8> {
+		let bytes = self.read_chunk::<1>()?;
+		Ok(bytes[0])
+	}
+
 	/// Takes the next 2 bytes as a little-endian signed integer.
 	pub fn read_i16(&mut self) -> Result<i16> {
 		let bytes = self.read_chunk::<2>()?;
 		Ok(i16::from_le_bytes(bytes))
 	}
 
+	/// Takes the next 2 bytes as a little-endian unsigned integer.
+	pub fn read_u16(&mut self) -> Result<u16> {
+		let bytes = self.read_chunk::<2>()?;
+		Ok(u16::from_le_bytes(bytes))
+	}
+
 	/// Takes the next 4 bytes as a little-endian signed integer.
 	pub fn read_i32(&mut self) -> Result<i32> {
 		let bytes = self.read_chunk::<4>()?;
 		Ok(i32::from_le_bytes(bytes))
+	}
+
+	/// Takes the next 4 bytes as a little-endian unsigned integer.
+	pub fn read_u32(&mut self) -> Result<u32> {
+		let bytes = self.read_chunk::<4>()?;
+		Ok(u32::from_le_bytes(bytes))
 	}
 
 	/// Checks if there are unread bytes remaining.
@@ -69,6 +97,25 @@ impl<'a> LumpParser<'a> {
 #[cfg(test)]
 mod tests {
 	use super::*;
+
+	#[test]
+	fn read_padding_ok() {
+		let mut parser = LumpParser::new(b"01234567");
+
+		parser.read_padding(1).unwrap();
+		parser.read_padding(2).unwrap();
+		parser.read_padding(3).unwrap();
+		assert_eq!(parser.read_slice(2).unwrap(), b"67");
+	}
+
+	#[test]
+	fn read_padding_err() {
+		let mut parser = LumpParser::new(b"01234567");
+
+		assert_eq!(parser.read_slice(7).unwrap(), b"0123456");
+		assert_eq!(parser.read_padding(2).unwrap_err(), ParseError::EndOfLump);
+		assert_eq!(parser.read_padding(1).unwrap_err(), ParseError::EndOfLump);
+	}
 
 	#[test]
 	fn read_slice_ok() {
@@ -107,6 +154,17 @@ mod tests {
 	}
 
 	#[test]
+	fn read_u8() {
+		let data = [0u8, 42u8, u8::MAX];
+		let mut parser = LumpParser::new(&data);
+
+		assert_eq!(parser.read_u8().unwrap(), 0);
+		assert_eq!(parser.read_u8().unwrap(), 42);
+		assert_eq!(parser.read_u8().unwrap(), u8::MAX);
+		assert_eq!(parser.read_u8().unwrap_err(), ParseError::EndOfLump);
+	}
+
+	#[test]
 	fn read_i16() {
 		let data = [
 			0i16.to_le_bytes(),
@@ -121,6 +179,23 @@ mod tests {
 		assert_eq!(parser.read_i16().unwrap(), 42);
 		assert_eq!(parser.read_i16().unwrap(), i16::MAX);
 		assert_eq!(parser.read_i16().unwrap(), i16::MIN);
+		assert_eq!(parser.read_i16().unwrap_err(), ParseError::EndOfLump);
+	}
+
+	#[test]
+	fn read_u16() {
+		let data = [
+			0u16.to_le_bytes(),
+			42u16.to_le_bytes(),
+			u16::MAX.to_le_bytes(),
+		]
+		.concat();
+		let mut parser = LumpParser::new(&data);
+
+		assert_eq!(parser.read_u16().unwrap(), 0);
+		assert_eq!(parser.read_u16().unwrap(), 42);
+		assert_eq!(parser.read_u16().unwrap(), u16::MAX);
+		assert_eq!(parser.read_u16().unwrap_err(), ParseError::EndOfLump);
 	}
 
 	#[test]
@@ -138,6 +213,23 @@ mod tests {
 		assert_eq!(parser.read_i32().unwrap(), 42);
 		assert_eq!(parser.read_i32().unwrap(), i32::MAX);
 		assert_eq!(parser.read_i32().unwrap(), i32::MIN);
+		assert_eq!(parser.read_i32().unwrap_err(), ParseError::EndOfLump);
+	}
+
+	#[test]
+	fn read_u32() {
+		let data = [
+			0u32.to_le_bytes(),
+			42u32.to_le_bytes(),
+			u32::MAX.to_le_bytes(),
+		]
+		.concat();
+		let mut parser = LumpParser::new(&data);
+
+		assert_eq!(parser.read_u32().unwrap(), 0);
+		assert_eq!(parser.read_u32().unwrap(), 42);
+		assert_eq!(parser.read_u32().unwrap(), u32::MAX);
+		assert_eq!(parser.read_u32().unwrap_err(), ParseError::EndOfLump);
 	}
 
 	#[test]
